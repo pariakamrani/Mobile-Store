8ff956c631030ab3e9f1f8866da5e370c21645ae
pariakamrani <kamraniparia@gmail.com>
2025-11-22 04:11:23 -0800

    Updates

--- api/iphone.js
+++ api/iphone.js
@@ -3,47 +3,47 @@ export const  products = [
             id: 1,
             title: "گوشی موبایل اپل مدل iPhone 17 Pro Max ZAA تک سیم کارت  ",
             price: 64000000,
-            img: "/img/p1.webp"
+            img: "./img/p1.webp"
         },
         {
             id: 2,
             title:"گوشی اپل iPhone 13 Pro Max ZAA رجیستر‌شده دو سیم‌کارت 256 گیگابایت",
             price: 75000000,
-            img: "/img/p2.webp"
+            img: "./img/p2.webp"
         },
         {
             id: 3,
             title: "گوشی اپل iPhone 13 Pro Max ZAA رجیستر‌شده دو سیم‌کارت 256 گیگابایت",
             price: 119000000,
-            img: "/img/p3.webp"
+            img: "./img/p3.webp"
         },
         {
             id: 4,
             title: "گوشی اپل iPhone 13 CH رجیسترشده دو سیم‌کارت 256 گیگابایت با رم 4 گیگابایت",
             price: 124000000,
-            img: "/img/p4.webp"
+            img: "./img/p4.webp"
         }, {
             id: 5,
             title: "گوشی موبایل اپل مدل iPhone 16 Pro ZAA",
             price: 150000000,
-            img: "/img/p5.webp"
+            img: "./img/p5.webp"
         },
         {
             id: 6,
             title: "گوشی موبایل اپل مدل iPhone XR A2108 دو سیم کارت ظرفیت 256 گیگابایت",
             price: 60000000,
-            img: "/img/p6.webp"
+            img: "./img/p6.webp"
         },
         {
             id: 7,
             title: "گوشی موبایل اپل مدل iPhone 16 Plus CH دو سیم کارت ظرفیت 512 ",
             price: 95000000,
-            img: "/img/p7.webp"
+            img: "./img/p7.webp"
         },
         {
             id: 8,
             title:"گوشی موبایل اپل مدل iPhone 16 CH دو سیم کارت ظرفیت 256 گیگابایت" ,
             price: 95000000,
-            img: "/img/p8.webp"
+            img: "./img/p8.webp"
         },
     ]
